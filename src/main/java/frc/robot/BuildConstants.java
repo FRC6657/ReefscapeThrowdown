7f3221f693d1ca16b2f1796ec641d2dd2f97c0b6
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-09-17 13:19:41 -0700

    branch init

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Lotus25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "c11ffbf2392159c1b78ddf3423c3f3d88c30481a";
-  public static final String GIT_DATE = "2025-09-16 18:13:03 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-09-16 18:42:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1758062529941L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "ac775e6e6b3ac6c093ca44a2ee48aeb602494717";
+  public static final String GIT_DATE = "2025-09-16 18:42:42 EDT";
+  public static final String GIT_BRANCH = "armExtension";
+  public static final String BUILD_DATE = "2025-09-17 16:18:38 EDT";
+  public static final long BUILD_UNIX_TIME = 1758140318449L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
