650dda913ef9d9ebabfbd7ac20531f92c6af1551
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-09-17 13:32:17 -0700

    install phoenix 5 for TalonSRX

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Lotus25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "ac775e6e6b3ac6c093ca44a2ee48aeb602494717";
-  public static final String GIT_DATE = "2025-09-16 18:42:42 EDT";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "7f3221f693d1ca16b2f1796ec641d2dd2f97c0b6";
+  public static final String GIT_DATE = "2025-09-17 16:19:41 EDT";
   public static final String GIT_BRANCH = "armExtension";
-  public static final String BUILD_DATE = "2025-09-17 16:18:38 EDT";
-  public static final long BUILD_UNIX_TIME = 1758140318449L;
+  public static final String BUILD_DATE = "2025-09-17 16:32:02 EDT";
+  public static final long BUILD_UNIX_TIME = 1758141122320L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
