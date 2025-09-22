1e9f1c07ee8aea975489849e2a780970ff8fe978
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-09-22 12:11:58 -0700

    finish armExtensionIO (that was easy)

--- src/main/java/frc/robot/subsystems/armExtension/armExtensionIO.java
+++ src/main/java/frc/robot/subsystems/armExtension/armExtensionIO.java
@@ -1,5 +1,15 @@
 package frc.robot.subsystems.armExtension;
 
-public class armExtensionIO {
+import org.littletonrobotics.junction.AutoLog;
+
+public interface armExtensionIO {
+    @AutoLog
+    public static class armExtensionIOInputs {
+        public double temp = 0; // in Celcius
+        public double volts = 0;
+        public double amps = 0;
+    }
     
+    public default void updateInputs(armExtensionIOInputs inputs) {}
+    public default void setVoltage(double volts) {}
 }
