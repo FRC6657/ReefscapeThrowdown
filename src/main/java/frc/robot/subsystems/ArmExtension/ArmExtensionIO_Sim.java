8bf9e74a625f6c478902e43ec6e7350c10e59f79
Iskand <tajfentabi@gmail.com>
2025-09-30 13:14:16 -0700

    Override default commands in ArmExtensionIO_Sim

--- src/main/java/frc/robot/subsystems/ArmExtension/ArmExtensionIO_Sim.java
+++ src/main/java/frc/robot/subsystems/ArmExtension/ArmExtensionIO_Sim.java
@@ -1,5 +1,9 @@
 package frc.robot.subsystems.armextension;
 
 public class ArmExtensionIO_Sim implements ArmExtensionIO{
-    
+    @Override
+    public void updateInputs(ArmExtensionIOInputs inputs){}
+
+    @Override
+    public void go(double dutycyc){}
 }
