5950af7333b91f66c22dc4e0d35d57f4ef1a763f
FranklinFFR <somewhat.professional.nerd@gmail.com>
2025-10-03 09:49:35 -0700

    ran succesfull simulation

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Lotus25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "37e56b9b00a29bc44ab45ed1cf184943a819b589";
-  public static final String GIT_DATE = "2025-09-30 16:34:23 EDT";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "2a0895bbc77037dbefa1b553309e9679283e07c4";
+  public static final String GIT_DATE = "2025-09-30 16:59:39 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-09-30 16:36:09 EDT";
-  public static final long BUILD_UNIX_TIME = 1759264569825L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-10-03 12:43:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1759509823492L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
