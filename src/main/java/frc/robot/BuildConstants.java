51438a1ae0d1289e4ae35702f8dabc8388afe577
Fabian-Anguiano <fabian.anguiano@uphsconnect.org>
2025-10-07 16:05:53 -0700

    Initialized Arm Pivot Subsystems

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Lotus25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 13;
-  public static final String GIT_SHA = "2a0895bbc77037dbefa1b553309e9679283e07c4";
-  public static final String GIT_DATE = "2025-09-30 16:59:39 EDT";
+  public static final int GIT_REVISION = 14;
+  public static final String GIT_SHA = "5950af7333b91f66c22dc4e0d35d57f4ef1a763f";
+  public static final String GIT_DATE = "2025-10-03 12:49:35 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-10-03 12:43:43 EDT";
-  public static final long BUILD_UNIX_TIME = 1759509823492L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-10-07 19:01:09 EDT";
+  public static final long BUILD_UNIX_TIME = 1759878069206L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
