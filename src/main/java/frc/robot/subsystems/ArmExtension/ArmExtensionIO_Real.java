92ee8e5502de5def5d02a9ad543c22f4636e167d
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-10-07 16:37:33 -0700

    update to TalonSRX

--- src/main/java/frc/robot/subsystems/ArmExtension/ArmExtensionIO_Real.java
+++ src/main/java/frc/robot/subsystems/ArmExtension/ArmExtensionIO_Real.java
@@ -1,14 +1,14 @@
 package frc.robot.subsystems.armextension;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import frc.robot.Constants.ArmConstants;
 
 public class ArmExtensionIO_Real implements ArmExtensionIO {
-        private PWMSparkMax motor;
+        private PWMTalonSRX motor;
 
         @Override
         public void updateInputs(ArmExtensionIOInputs inputs) {
-                motor = new PWMSparkMax(ArmConstants.kArmExtend);
+                motor = new PWMTalonSRX(ArmConstants.kArmExtend);
         }
 
         @Override
