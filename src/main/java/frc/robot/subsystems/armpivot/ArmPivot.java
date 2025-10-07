647d9c83003cefb17ca47ff6577fc413dd56aa2e
Fabian-Anguiano <fabian.anguiano@uphsconnect.org>
2025-10-07 16:28:08 -0700

    ArmPivot

--- src/main/java/frc/robot/subsystems/armpivot/ArmPivot.java
+++ src/main/java/frc/robot/subsystems/armpivot/ArmPivot.java
@@ -1,5 +1,33 @@
 package frc.robot.subsystems.armpivot;
 
-public class ArmPivot {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
+
+public class ArmPivot extends SubsystemBase {
     
+    private final ArmPivot io;
+    private final ArmPivotIOInputsAutoLogged inputs = new ArmPivotIOInputsAutoLogged();
+
+    public ArmPivot(ArmPivotIO io) {
+        this.io = io;
+    }
+
+    public Command changeSetpoint(double setpoint) {
+        return this.runOnce(
+            () -> {
+                io.changeSetpoint(setpoint);
+            }
+        );
+    }
+
+    public void setpoint(double setpoint) {
+        io.changeSetpoint(setpoint);
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("ArmPivot", inputs);
+    }
 }
