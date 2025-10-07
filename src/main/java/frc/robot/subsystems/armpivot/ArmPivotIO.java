de173810955ebbce8ce60a96570c77ecbd29a201
Anmol-Cheema <anmol.cheema@uphsconnect.org>
2025-10-07 16:24:58 -0700

    ArmPivotIO

--- src/main/java/frc/robot/subsystems/armpivot/ArmPivotIO.java
+++ src/main/java/frc/robot/subsystems/armpivot/ArmPivotIO.java
@@ -1,5 +1,23 @@
 package frc.robot.subsystems.armpivot;
 
-public class ArmPivotIO {
-    
-}
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ArmPivotIO {
+
+    @AutoLog 
+    public static class OuttakeIOInputs {
+        public double kSetpoint = 0.0;
+        public double kVelocity = 0.0;
+        public double kTemp = 0.0;
+        public double kVoltage = 0.0;
+        public double kCurrent = 0.0;
+    }
+
+    public default void updateInputs(OuttakeIOInputs inputs) {}
+
+    public default boolean getBeamBroken(){
+        return true;
+    }
+
+    public default void changeSetpoint(double setpoint) {}
+} 
