24f6129c4426814c7df451dba5b4bdf745e2a699
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-10-07 17:00:41 -0700

    hopefully done with sim code, needs review

--- src/main/java/frc/robot/subsystems/ArmExtension/ArmExtensionIO_Sim.java
+++ src/main/java/frc/robot/subsystems/ArmExtension/ArmExtensionIO_Sim.java
@@ -1,9 +1,38 @@
 package frc.robot.subsystems.armextension;
 
-public class ArmExtensionIO_Sim implements ArmExtensionIO{
-    @Override
-    public void updateInputs(ArmExtensionIOInputs inputs){}
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
+import frc.robot.Constants;
 
-    @Override
-    public void setSpeed(double dutycyc){}
+public class ArmExtensionIO_Sim implements ArmExtensionIO{
+
+    private double voltage = 0;
+
+  private DCMotorSim armExtensionSim =
+      new DCMotorSim(
+          LinearSystemId.createDCMotorSystem(
+              DCMotor.getNEO(1), 0.0001, 1),
+          DCMotor.getNEO(1));
+
+  public ArmExtensionIO_Sim() {}
+
+  @Override
+  public void updateInputs(ArmExtensionIOInputs inputs) {
+
+    // update sim
+    armExtensionSim.update(1 / Constants.CodeConstants.kMainLoopFrequency);
+
+    // inputs
+    inputs.volts = voltage; // volts
+  }
+
+  @Override
+  public void setSpeed(double volts) {
+    voltage = MathUtil.clamp(volts, -12, 12);
+    armExtensionSim.setInput(voltage);
+  }
 }
