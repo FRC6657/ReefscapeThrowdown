f6a3c20cdf5dcd2c76d99394d84ccd2ab2f405a6
madisonh0609 <madison.hutchinson@uphsconnect.org>
2025-10-08 17:03:36 -0700

    finished sim code

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Lotus25";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 19;
-  public static final String GIT_SHA = "4d75e90da1b6a5bd2b26045ba741c82251d14f7f";
-  public static final String GIT_DATE = "2025-10-08 19:15:58 EDT";
-  public static final String GIT_BRANCH = "clawWheels";
-  public static final String BUILD_DATE = "2025-10-08 19:25:05 EDT";
-  public static final long BUILD_UNIX_TIME = 1759965905057L;
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "9bef21605233048451dd74fed47fa6cd090991b4";
+  public static final String GIT_DATE = "2025-10-08 19:36:44 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-10-08 20:03:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1759968183632L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/clawwheels/ClawWheelsIO_Sim.java
+++ src/main/java/frc/robot/subsystems/clawwheels/ClawWheelsIO_Sim.java
@@ -1,21 +1,30 @@
 package frc.robot.subsystems.clawwheels;
 
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
 public class ClawWheelsIO_Sim implements ClawWheelsIO{
-    private double clawWheelsVoltage = 0;
+    private double voltage = 0;
+    double setpoint = 0.0;
+    double speed = 0.0;
 
     private DCMotorSim clawWheelSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getNEO(1), 0.0001, 1), DCMotor.getNEO(1));
 
 
-
 @Override
   public void updateInputs(ClawWheelsIOInputs inputs) {
-    
+    inputs.kTemp = 0;
     inputs.kCurrent = clawWheelSim.getCurrentDrawAmps(); 
-    
+    inputs.kVoltage = voltage;
+    inputs.kSetpoint = setpoint;
   }
-
+ 
+  
+  @Override
+  public void setSpeed(double setpoint){
+     setpoint = speed;
+  }
 }
