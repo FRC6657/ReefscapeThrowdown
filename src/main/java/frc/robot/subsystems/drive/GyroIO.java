659f2bdeb4cea26a701dabea7fde8277163abc46
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-10-21 16:31:54 -0700

    fix everything hopefully (#6)
    * Fix main (real)
    
    * Fix everything (hopefully)

--- src/main/java/frc/robot/subsystems/drive/GyroIO.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIO.java
@@ -9,8 +9,8 @@ public interface GyroIO {
   @AutoLog
   public static class GyroIOInputs {
     public boolean connected = false;
-    public Rotation2d yawPosition = new Rotation2d();
-    public double yawVelocityRadPerSec = 0.0;
+    public double yawPosition = 0.0;
+    public double yawVelocityRadPerSec = 0.0; //TODO: We dont acutlaly use this anywhere, might aswell just axe it.
   }
 
   public default void updateInputs(GyroIOInputs inputs) {}
