0aec138079d274355a36755c8154394f1ea78993
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-10-28 15:52:06 -0700

    Change motor from PWM to CAN

--- src/main/java/frc/robot/subsystems/arm/extension/armExtensionIO_Real.java
+++ src/main/java/frc/robot/subsystems/arm/extension/armExtensionIO_Real.java
@@ -1,18 +1,19 @@
 package frc.robot.subsystems.arm.extension;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.Constants.ArmConstants;
 
 public class ArmExtensionIO_Real implements ArmExtensionIO {
-   private PWMTalonSRX motor; // TODO should be TalonSRX since its being ran with CAN not PWM.
+   private TalonSRX motor;
 
    @Override
-   public void updateInputs(armExtensionIOInputs inputs) {
-      motor = new PWMTalonSRX(ArmConstants.kArmExtend);
+   public void updateInputs(ArmExtensionIOInputs inputs) {
+      motor = new TalonSRX(ArmConstants.kArmExtend);
    }
 
    @Override
    public void setSpeed(double dutycyc) {
-      motor.set(dutycyc);
+      motor.set(TalonSRXControlMode.PercentOutput, dutycyc);
    }
 }
