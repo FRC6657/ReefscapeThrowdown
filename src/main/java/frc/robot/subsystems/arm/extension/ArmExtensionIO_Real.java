e3d713c197c1d2cf7f0d4a5f3e91839abded1398
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-10-29 15:56:47 -0700

    Fix CANID in ArmExtensionIO_Real (wasn't set to proper CANID) (#9)

--- src/main/java/frc/robot/subsystems/arm/extension/ArmExtensionIO_Real.java
+++ src/main/java/frc/robot/subsystems/arm/extension/ArmExtensionIO_Real.java
@@ -2,14 +2,14 @@ package frc.robot.subsystems.arm.extension;
 
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.*;
 
 public class ArmExtensionIO_Real implements ArmExtensionIO {
   private TalonSRX motor;
 
   @Override
   public void updateInputs(ArmExtensionIOInputs inputs) {
-    motor = new TalonSRX(ArmConstants.kArmExtend);
+    motor = new TalonSRX(CANID.kClawExtension);
   }
 
   @Override
