32e490f236f64b229b3a8a8c32f1db4e4f3d259f
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2025-10-31 16:52:00 -0700

    basic superstructure setup

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -3,16 +3,30 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
+import frc.robot.subsystems.arm.claw.ClawWheels;
+import frc.robot.subsystems.arm.extension.ArmExtension;
 import frc.robot.subsystems.arm.pivot.ArmPivot;
+import frc.robot.subsystems.drive.MAXSwerve;
+
 import org.littletonrobotics.junction.AutoLogOutput;
 
 public class Superstructure {
 
+  private MAXSwerve drivebase;
+  private ArmExtension armext;
+  private ClawWheels claw;
   private ArmPivot pivot;
 
   private String extentionState = "retracted";
 
-  public Superstructure(ArmPivot pivot) {
+  public Superstructure(
+      MAXSwerve drivebase,
+      ArmExtension armext,
+      ClawWheels claw,
+      ArmPivot pivot) {
+    this.drivebase = drivebase;
+    this.armext = armext;
+    this.claw = claw;
     this.pivot = pivot;
   }
 
