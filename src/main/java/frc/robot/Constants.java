FRC6657/ReefscapeThrowdown

commit 956c8d3fadafccb3f6405dd446c0142e8bed2281

added operator buttons

src/main/java/frc/robot/Robot.java
@@ -10,15 +10,10 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.MAXSwerveConstants;
+import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.arm.claw.ClawWheels;
-import frc.robot.subsystems.arm.claw.ClawWheelsIO_Real;
-import frc.robot.subsystems.arm.claw.ClawWheelsIO_Sim;
 import frc.robot.subsystems.arm.extension.ArmExtension;
-import frc.robot.subsystems.arm.extension.ArmExtensionIO_Real;
-import frc.robot.subsystems.arm.extension.ArmExtensionIO_Sim;
 import frc.robot.subsystems.arm.pivot.ArmPivot;
-import frc.robot.subsystems.arm.pivot.ArmPivotIO_Real;
-import frc.robot.subsystems.arm.pivot.ArmPivotIO_Sim;
 import frc.robot.subsystems.drive.GyroIO;
 import frc.robot.subsystems.drive.GyroIO_Real;
 import frc.robot.subsystems.drive.MAXSwerve;
@@ -26,8 +21,7 @@ import frc.robot.subsystems.drive.MAXSwerveIO;
 import frc.robot.subsystems.drive.MAXSwerveIO_Real;
 import frc.robot.subsystems.drive.MAXSwerveIO_Sim;
 import frc.robot.subsystems.hopper.Hopper;
-import frc.robot.subsystems.hopper.HopperIO_Real;
-import frc.robot.subsystems.hopper.HopperIO_Sim;
+
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -37,6 +31,12 @@ import org.littletonrobotics.junction.wpilog.WPILOGReader;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 public class Robot extends LoggedRobot {
+
+  private ArmExtension armext;
+  private ClawWheels claw;
+  private ArmPivot pivot;
+  private Hopper hopper;
+  private Superstructure superstructure;
 
   public static enum RobotMode {
     SIM,
@@ -53,6 +53,7 @@ public class Robot extends LoggedRobot {
 
   // Driver Controllers
   private CommandXboxController controller = new CommandXboxController(0);
+  private CommandXboxController operator = new CommandXboxController(1);
 
   // Subsystems
   private MAXSwerve drivebase =
@@ -71,13 +72,6 @@ public class Robot extends LoggedRobot {
                 new MAXSwerveIO_Sim(),
                 new MAXSwerveIO_Sim()
               });
-  private ArmPivot pivot =
-      new ArmPivot(mode == RobotMode.REAL ? new ArmPivotIO_Real() : new ArmPivotIO_Sim());
-  private ArmExtension armext =
-      new ArmExtension(mode == RobotMode.REAL ? new ArmExtensionIO_Real() : new ArmExtensionIO_Sim());
-  private ClawWheels claw =
-      new ClawWheels(mode == RobotMode.REAL ? new ClawWheelsIO_Real() : new ClawWheelsIO_Sim());
-  private Hopper hopper = new Hopper(mode == RobotMode.REAL ? new HopperIO_Real() : new HopperIO_Sim());
 
   @SuppressWarnings(value = "resource")
   @Override
@@ -120,7 +114,11 @@ public class Robot extends LoggedRobot {
                         * DriveConstants.kMaxAngularVelocity
                         * 0.25)));
 
-    autoChooser.addDefaultOption("None", Commands.none());
+    operator.a().onTrue(superstructure.selectPivotHeight(1));
+    operator.b().onTrue(superstructure.selectPivotHeight(2));
+    operator.y().onTrue(superstructure.selectPivotHeight(3));
+    operator.leftTrigger().onTrue(superstructure.ready());
+    operator.rightTrigger().onTrue(superstructure.Intake());
   }
 
   @Override

commit 4a3be6dd960272f966899275660c435811f6e586

Superstructure implementation (#11)
* Update from main

* basic superstructure setup

* spotless

* raise Pivot added

* raise Pivot added

* Added pivot hieght selection

* Ready robot command

* Spotless

* probably found the drive issue

* Starter setpoints from cad

* Add hopper to superstructure & add hopper to Ready

* reset hopper speed after waiting (forgot)

* remove hopper from ready

* Added dedicated commands to retract and extend the claw, to properly track 3d pose.

* added hopper to superstructure constructor

* Spotless and Current Limits

* Add intake command

* spotless complained about this

* score coral command done

* rename "scoreCoralCommand" to "Score"

* spotless

* add the robot subsystems to Robot.java

* spotless

* switch to use built-in enum for RobotType

* added operator buttons

* fix issues caused by previous commit 956c8d3fadafccb3f6405dd446c0142e8bed2281

* changed to driver

* Added Driver Buttons

* i don't even know atp

* no extension during score

* stuff

* Spotless

---------

Co-authored-by: Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
Co-authored-by: FranklinFFR <somewhat.professional.nerd@gmail.com>
Co-authored-by: Andrew-6657 <andrew.frankovic@uphsconnect.org>
Co-authored-by: Andrew-6657 <andrew@frankovic.com>
Co-authored-by: madisonh0609 <madison.hutchinson@uphsconnect.org>
Co-authored-by: Fabian-Anguiano <fabian.anguiano@uphsconnect.org>

src/main/java/frc/robot/Constants.java
@@ -38,6 +38,7 @@ public class Constants {
     public static final int kPivot = 10;
     public static final int kClawWheels = 11;
     public static final int kClawExtension = 12;
+    public static final int kHopper = 13;
   }
 
   public static final class AutoConstants {

src/main/java/frc/robot/Robot.java
@@ -6,18 +6,28 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.MAXSwerveConstants;
+import frc.robot.subsystems.Superstructure;
+import frc.robot.subsystems.arm.claw.ClawWheels;
+import frc.robot.subsystems.arm.claw.ClawWheelsIO_Real;
+import frc.robot.subsystems.arm.claw.ClawWheelsIO_Sim;
+import frc.robot.subsystems.arm.extension.ArmExtension;
+import frc.robot.subsystems.arm.extension.ArmExtensionIO_Real;
+import frc.robot.subsystems.arm.extension.ArmExtensionIO_Sim;
 import frc.robot.subsystems.arm.pivot.ArmPivot;
 import frc.robot.subsystems.arm.pivot.ArmPivotIO_Real;
+import frc.robot.subsystems.arm.pivot.ArmPivotIO_Sim;
 import frc.robot.subsystems.drive.GyroIO;
 import frc.robot.subsystems.drive.GyroIO_Real;
 import frc.robot.subsystems.drive.MAXSwerve;
 import frc.robot.subsystems.drive.MAXSwerveIO;
 import frc.robot.subsystems.drive.MAXSwerveIO_Real;
 import frc.robot.subsystems.drive.MAXSwerveIO_Sim;
+import frc.robot.subsystems.hopper.Hopper;
+import frc.robot.subsystems.hopper.HopperIO_Real;
+import frc.robot.subsystems.hopper.HopperIO_Sim;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -28,6 +38,8 @@ import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
 public class Robot extends LoggedRobot {
 
+  private Superstructure superstructure;
+
   public static enum RobotMode {
     SIM,
     REPLAY,
@@ -36,14 +48,14 @@ public class Robot extends LoggedRobot {
 
   // Control the mode of the robot
   public static final RobotMode mode = Robot.isReal() ? RobotMode.REAL : RobotMode.SIM;
-  // public static final RobotMode mode = RobotMode.REPLAY;
 
   // Auto Command
   private final LoggedDashboardChooser<Command> autoChooser =
       new LoggedDashboardChooser<>("Auto Chooser");
 
   // Driver Controllers
-  private CommandXboxController controller = new CommandXboxController(0);
+  private CommandXboxController driver = new CommandXboxController(0);
+  private CommandXboxController operator = new CommandXboxController(1);
 
   // Subsystems
   private ArmPivot pivot = new ArmPivot(new ArmPivotIO_Real());
@@ -64,6 +76,15 @@ public class Robot extends LoggedRobot {
                 new MAXSwerveIO_Sim(),
                 new MAXSwerveIO_Sim()
               });
+  private ArmPivot pivot =
+      new ArmPivot(mode == RobotMode.REAL ? new ArmPivotIO_Real() : new ArmPivotIO_Sim());
+  private ArmExtension armext =
+      new ArmExtension(
+          mode == RobotMode.REAL ? new ArmExtensionIO_Real() : new ArmExtensionIO_Sim());
+  private ClawWheels claw =
+      new ClawWheels(mode == RobotMode.REAL ? new ClawWheelsIO_Real() : new ClawWheelsIO_Sim());
+  private Hopper hopper =
+      new Hopper(mode == RobotMode.REAL ? new HopperIO_Real() : new HopperIO_Sim());
 
   @SuppressWarnings(value = "resource")
   @Override
@@ -96,26 +117,26 @@ public class Robot extends LoggedRobot {
         drivebase.runVelocityFieldRelative(
             () ->
                 new ChassisSpeeds(
-                    -MathUtil.applyDeadband(controller.getLeftY(), 0.05)
+                    -MathUtil.applyDeadband(driver.getLeftY(), 0.05)
                         * MAXSwerveConstants.kMaxDriveSpeed
                         * 0.25,
-                    -MathUtil.applyDeadband(controller.getLeftX(), 0.15)
+                    -MathUtil.applyDeadband(driver.getLeftX(), 0.15)
                         * MAXSwerveConstants.kMaxDriveSpeed
                         * 0.25,
-                    -MathUtil.applyDeadband(controller.getRightX(), 0.15)
+                    -MathUtil.applyDeadband(driver.getRightX(), 0.15)
                         * DriveConstants.kMaxAngularVelocity
                         * 0.25)));
 
-    autoChooser.addDefaultOption("None", Commands.none());
-    // controller
-    //     .a()
-    //     .whileTrue(
-    //         Commands.runOnce(
-    //             () -> drivebase.setPose(new Pose2d(0, 0, new Rotation2d(0))), drivebase));
-    // controller.b().whileTrue(drivebase.goToPose(new Pose2d(0, 0, new Rotation2d(0))));
+    operator.a().onTrue(superstructure.selectPivotHeight(1));
+    operator.b().onTrue(superstructure.selectPivotHeight(2));
+    operator.y().onTrue(superstructure.selectPivotHeight(3));
+    operator.leftTrigger().onTrue(superstructure.ready());
+    operator.rightTrigger().onTrue(superstructure.Intake());
+    operator.leftBumper().onTrue(superstructure.HomeRobot());
 
-    controller.a().onTrue(pivot.changeSetpoint(0));
-    controller.b().onTrue(pivot.changeSetpoint(-90));
+    driver.leftBumper().onTrue(superstructure.HomeRobot());
+    driver.rightTrigger().onTrue(superstructure.raisePivot());
+    driver.leftTrigger().onTrue(superstructure.Score());
   }
 
   @Override

src/main/java/frc/robot/subsystems/Superstructure.java
@@ -3,17 +3,52 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.subsystems.arm.claw.ClawWheels;
+import frc.robot.subsystems.arm.extension.ArmExtension;
 import frc.robot.subsystems.arm.pivot.ArmPivot;
+import frc.robot.subsystems.drive.MAXSwerve;
+import frc.robot.subsystems.hopper.Hopper;
 import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
 
 public class Superstructure {
 
+  private MAXSwerve drivebase;
+  private ArmExtension armext;
+  private ClawWheels claw;
   private ArmPivot pivot;
+  private Hopper hopper;
 
   private String extentionState = "retracted";
 
-  public Superstructure(ArmPivot pivot) {
+  // Array for easily grabbing setpoint angles.
+  private double[] pivotSetpoints = {
+    Constants.ArmPivotConstants.initialSetpoint,
+    -17, // L1 Ready
+    12, // L2 Ready
+    55, // L3 Ready
+    -17, // L1 Score
+    -25, // L2 Score
+    17, // L3 Score
+  };
+
+  @AutoLogOutput(key = "RobotStates/Elevator Level")
+  private int pivotLevel = 2; // Selected Reef Level
+
+  public Superstructure(
+      MAXSwerve drivebase, ArmExtension armext, ClawWheels claw, ArmPivot pivot, Hopper hopper) {
+    this.drivebase = drivebase;
+    this.armext = armext;
+    this.claw = claw;
     this.pivot = pivot;
+    this.hopper = hopper;
+  }
+
+  public Command logMessage(String message) {
+    return Commands.runOnce(() -> Logger.recordOutput("Command Log", message));
   }
 
   @AutoLogOutput(key = "Superstructure/RobotPoses")
@@ -35,5 +70,71 @@ public class Superstructure {
                   * Units.inchesToMeters(fakeExtentionDistance),
           new Rotation3d(0, -Units.degreesToRadians(pivot.getPosition()) - Math.PI / 2, 0))
     };
+  }
+
+  public Command extendClaw() {
+    return Commands.sequence(
+        armext.setSpeed(0.2),
+        Commands.waitSeconds(0.5),
+        armext.setSpeed(0),
+        Commands.runOnce(
+            () -> {
+              extentionState = "extended";
+            }));
+  }
+
+  public Command retractClaw() {
+    return Commands.sequence(
+        armext.setSpeed(-0.2),
+        Commands.waitSeconds(0.5),
+        armext.setSpeed(0),
+        Commands.runOnce(
+            () -> {
+              extentionState = "retracted";
+            }));
+  }
+
+  public Command ready() {
+    return Commands.sequence(
+        claw.changeSetpoint(-0.3), extendClaw(), claw.changeSetpoint(0), retractClaw());
+  }
+
+  public Command selectPivotHeight(int height) {
+    return Commands.runOnce(() -> pivotLevel = height)
+        .andThen(logMessage("Selected Pivot Height: " + height));
+  }
+
+  // Change Elevator Setpoint to the selected reef level.
+  public Command raisePivot() {
+    return pivot
+        .changeSetpoint(() -> pivotSetpoints[pivotLevel])
+        .andThen(
+            logMessage(
+                "Pivot Setpoint Changed To: "
+                    + pivotSetpoints[pivotLevel]
+                    + " Reef Level: "
+                    + pivotLevel));
+  }
+
+  public Command HomeRobot() {
+    return Commands.sequence(
+        armext.setSpeed(0),
+        claw.changeSetpoint(0),
+        pivot.changeSetpoint(Constants.ArmPivotConstants.initialSetpoint));
+  }
+
+  public Command Intake() {
+    return Commands.sequence(
+        hopper.changeSetpoint(0.5), Commands.waitSeconds(1), hopper.changeSetpoint(0));
+  }
+
+  public Command Score() {
+    return Commands.sequence(
+        logMessage("Score"),
+        Commands.waitSeconds(0.5),
+        claw.changeSetpoint(0.5),
+        Commands.waitSeconds(0.5),
+        claw.changeSetpoint(0),
+        Commands.waitSeconds(0.5));
   }
 }

src/main/java/frc/robot/subsystems/arm/claw/ClawWheelsIO.java
@@ -6,11 +6,9 @@ public interface ClawWheelsIO {
 
   @AutoLog
   public static class ClawWheelsIOInputs {
-    // TODO: k is usually a prefix for constants. Change name.
     public double kSetpoint = 0.0;
-    public double kTemp = 0.0; // TODO: Probably dont need to log this
     public double kVoltage = 0.0;
-    public double kCurrent = 0.0; // TODO: Probably dont need to log this
+    public double kCurrent = 0.0;
   }
 
   public default void updateInputs(ClawWheelsIOInputs inputs) {}

src/main/java/frc/robot/subsystems/arm/claw/ClawWheelsIO_Real.java
@@ -1,31 +1,33 @@
 package frc.robot.subsystems.arm.claw;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.Constants;
 
 public class ClawWheelsIO_Real implements ClawWheelsIO {
 
-  private TalonSRX clawMotor = new TalonSRX(Constants.CANID.kClawWheels);
+  private TalonSRX clawMotor;
   private double setpoint = 0;
 
   public ClawWheelsIO_Real() {
+    clawMotor = new TalonSRX(Constants.CANID.kClawWheels);
+    clawMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 20, 20, 0));
     clawMotor.setNeutralMode(NeutralMode.Brake);
-    setSpeed(0);
+    clawMotor.setInverted(false);
   }
 
   @Override
   public void updateInputs(ClawWheelsIOInputs inputs) {
-    inputs.kCurrent = clawMotor.getSupplyCurrent(); // TODO: Probably dont need to log this
+    inputs.kCurrent = clawMotor.getSupplyCurrent();
     inputs.kVoltage = clawMotor.getMotorOutputVoltage();
     inputs.kSetpoint = setpoint;
+    clawMotor.set(TalonSRXControlMode.PercentOutput, setpoint);
   }
 
   @Override
   public void setSpeed(double speed) {
     setpoint = speed;
-    clawMotor.set(
-        TalonSRXControlMode.PercentOutput, setpoint); // TODO: This should be put in update inputs.
   }
 }

src/main/java/frc/robot/subsystems/arm/claw/ClawWheelsIO_Sim.java
@@ -15,7 +15,6 @@ public class ClawWheelsIO_Sim implements ClawWheelsIO {
 
   @Override
   public void updateInputs(ClawWheelsIOInputs inputs) {
-    inputs.kTemp = 0;
     inputs.kCurrent = clawWheelSim.getCurrentDrawAmps();
     inputs.kVoltage = voltage;
     inputs.kSetpoint = setpoint;

src/main/java/frc/robot/subsystems/arm/extension/ArmExtensionIO_Sim.java
@@ -18,11 +18,7 @@ public class ArmExtensionIO_Sim implements ArmExtensionIO {
 
   @Override
   public void updateInputs(ArmExtensionIOInputs inputs) {
-
-    // update sim
     armExtensionSim.update(1 / Constants.CodeConstants.kMainLoopFrequency);
-
-    // inputs
     inputs.volts = voltage; // volts
   }
 

src/main/java/frc/robot/subsystems/arm/extension/armExtensionIO_Real.java
@@ -8,16 +8,23 @@ import frc.robot.Constants.*;
 
 public class ArmExtensionIO_Real implements ArmExtensionIO {
   private TalonSRX motor;
+  private double setpoint = 0;
+
+  public ArmExtensionIO_Real() {
+    motor = new TalonSRX(CANID.kClawExtension);
+    motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 5, 5, 0));
+    motor.setInverted(false);
+    motor.setNeutralMode(NeutralMode.Brake);
+  }
 
   @Override
   public void updateInputs(ArmExtensionIOInputs inputs) {
-    motor = new TalonSRX(CANID.kClawExtension);
-    motor.setNeutralMode(NeutralMode.Brake);
-    motor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 5, 5, 0));
+    motor.set(TalonSRXControlMode.PercentOutput, setpoint);
+    inputs.volts = motor.getMotorOutputVoltage();
   }
 
   @Override
   public void setSpeed(double dutycyc) {
-    motor.set(TalonSRXControlMode.PercentOutput, dutycyc);
+    setpoint = dutycyc;
   }
 }

src/main/java/frc/robot/subsystems/arm/pivot/ArmPivot.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.arm.pivot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
 import org.littletonrobotics.junction.Logger;
 
 public class ArmPivot extends SubsystemBase {
@@ -20,7 +21,10 @@ public class ArmPivot extends SubsystemBase {
         });
   }
 
-  // TODO Needs clamp
+  public Command changeSetpoint(DoubleSupplier setpoint) {
+    return changeSetpoint(setpoint.getAsDouble());
+  }
+
   public void setpoint(double setpoint) {
     io.changeSetpoint(setpoint);
   }

src/main/java/frc/robot/subsystems/arm/pivot/ArmPivotIO_Real.java
@@ -55,15 +55,16 @@ public class ArmPivotIO_Real implements ArmPivotIO {
     inputs.kAcceleration = pivotMotor.getAcceleration().getValueAsDouble() * 360;
     inputs.kSetpoint = kSetpoint;
 
-    pivotMotor.setControl(motionMagicVoltage.withPosition(kSetpoint / 360));
+    pivotMotor.setControl(motionMagicVoltage);
   }
 
   @Override
   public void changeSetpoint(double setpoint) {
-    kSetpoint =
+    motionMagicVoltage.Position =
         MathUtil.clamp(
-            setpoint,
-            Constants.ArmPivotConstants.initialSetpoint,
-            Constants.ArmPivotConstants.maxStepoint);
+                setpoint,
+                Constants.ArmPivotConstants.initialSetpoint,
+                Constants.ArmPivotConstants.maxStepoint)
+            / 360;
   }
 }

src/main/java/frc/robot/subsystems/drive/GyroIO.java
@@ -9,8 +9,7 @@ public interface GyroIO {
   public static class GyroIOInputs {
     public boolean connected = false;
     public double yawPosition = 0.0;
-    public double yawVelocityRadPerSec =
-        0.0; // TODO: We dont acutlaly use this anywhere, might aswell just axe it.
+    public double yawVelocityRadPerSec = 0.0;
   }
 
   public default void updateInputs(GyroIOInputs inputs) {}

src/main/java/frc/robot/subsystems/drive/GyroIO_Real.java
@@ -11,7 +11,7 @@ public class GyroIO_Real implements GyroIO {
 
   private double yaw = pigeon.getYaw();
   private double previousYaw = yaw;
-  private double yawVelocity = 0.0; // TODO calculate velocity and update every tick
+  private double yawVelocity = 0.0;
 
   /** Gyro IO for real robot */
   public GyroIO_Real() {

src/main/java/frc/robot/subsystems/drive/MAXSwerveIO_Real.java
@@ -73,9 +73,7 @@ public class MAXSwerveIO_Real implements MAXSwerveIO {
     driveConfig.encoder.positionConversionFactor(
         MAXSwerveConstants.kDriveEncoderPositionFactor); // meters
     driveConfig.encoder.velocityConversionFactor(
-        MAXSwerveConstants.kDriveEncoderPositionFactor); // meters per second
-    // driveConfig.encoder.uvwMeasurementPeriod((int) (1000 / CodeConstants.kMainLoopFrequency));
-    // driveConfig.encoder.uvwAverageDepth(2);
+        MAXSwerveConstants.kDriveEncoderVelocityFactor); // meters per second
 
     turnConfig.absoluteEncoder.positionConversionFactor(
         MAXSwerveConstants.kTurnEncoderPositionFactor);

src/main/java/frc/robot/subsystems/hopper/HopperIO_Real.java
@@ -1,17 +1,20 @@
 package frc.robot.subsystems.hopper;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.Constants;
 
 public class HopperIO_Real implements HopperIO {
 
-  private TalonSRX hopperMotor = new TalonSRX(Constants.CANID.kClawWheels);
+  private TalonSRX hopperMotor = new TalonSRX(Constants.CANID.kHopper);
   private double setpoint = 0;
 
   public HopperIO_Real() {
     hopperMotor.setNeutralMode(NeutralMode.Coast);
+    hopperMotor.configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 20, 20, 0));
+    hopperMotor.setInverted(false);
     setSpeed(0);
   }
 
@@ -20,11 +23,11 @@ public class HopperIO_Real implements HopperIO {
     inputs.current = hopperMotor.getSupplyCurrent();
     inputs.voltage = hopperMotor.getMotorOutputVoltage();
     inputs.setpoint = setpoint;
+    hopperMotor.set(TalonSRXControlMode.PercentOutput, setpoint);
   }
 
   @Override
   public void setSpeed(double speed) {
     setpoint = speed;
-    hopperMotor.set(TalonSRXControlMode.PercentOutput, setpoint);
   }
 }